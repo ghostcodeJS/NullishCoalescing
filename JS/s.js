aec08d230522922a80bd7103e88a1a58d9c14d9e
YtrioSA <ytrios.a@gmail.com>
2023-09-29 15:49:10 -0300

    Operador de coalescência nula

---
+++ JS/s.js
@@ -0,0 +1,7 @@
+
+// a 84 anos...
+let seforNullLascouSeforUndefinedTbm = null;
+const bemPadrao = "Deu Pau Corre!";
+const rezoOTazo = ( seforNullLascouSeforUndefinedTbm !== null && seforNullLascouSeforUndefinedTbm !== undefined)
+            ? seforNullLascouSeforUndefinedTbm: bemPadrao;
+
